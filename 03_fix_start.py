80fb83fc136ee7720ab4a7de32eb43dad59598c7
Carol Bastos <46680271+carolbastos-cb@users.noreply.github.com>
2020-10-12 14:49:37 -0300

    Add files via upload

---
+++ 03_fix_start.py
@@ -0,0 +1,62 @@
+"""
+03. fix_start
+
+Dada uma string s, retorne uma string onde
+todas as ocorrências do primeiro caracter de s
+foram substituidas por '*', exceto a primeira.
+
+Exemplo: 'babble' retorna 'ba**le'
+
+Assuma que a string tem tamanho 1 ou maior.
+
+Dica: s.replace(stra, strb) retorna uma versão da string s
+onde todas as instancias de stra foram substituidas por strb.
+"""
+
+def fix_start(s):
+    """
+    Verifica qual a primeira letra da string s e substitui todas as ocorrências dessa letra na string por um *,
+    exceto pela primeira ocorrência.
+    param s: string
+    return: Retorna uma string com * no lugar das ocorrências da nova letra.
+    """
+    return ''.join(letter if index == 0 or letter != s[0] else '*' for index, letter in enumerate(s))
+
+
+"""
+ ---POSSIBILIDADE DE RESOLUÇÃO COM REPLACE---
+ 
+    substring = s[1:]
+    for letter in substring:
+        if letter == s[0]:
+            substring = substring.replace(letter, '*')
+    return ''.join([s[0], substring])
+"""
+
+
+
+# --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
+
+def test(f, in_, expected):
+    """
+    Executa a função f com o parâmetro in_ e compara o resultado com expected.
+    :return: Exibe uma mensagem indicando se a função f está correta ou não.
+    """
+    out = f(in_)
+
+    if out == expected:
+        sign = '✅'
+        info = ''
+    else:
+        sign = '❌'
+        info = f'e o correto é {expected!r}'
+
+    print(f'{sign} {f.__name__}({in_!r}) retornou {out!r} {info}')
+
+
+if __name__ == '__main__':
+    # Testes que verificam o resultado do seu código em alguns cenários.
+    test(fix_start, 'babble', 'ba**le')
+    test(fix_start, 'aardvark', 'a*rdv*rk')
+    test(fix_start, 'google', 'goo*le')
+    test(fix_start, 'donut', 'donut')
